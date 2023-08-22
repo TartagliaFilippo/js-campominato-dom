a6ecf02cf6ac45a618ae50da9f65cfb077bb835b
filippo199600 <pippotartaglia@gmail.com>
2023-08-22 16:10:14 +0200

    feat random number function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
-// collego il contenitore della griglia
-const gridElement = document.getElementById("grid");
-
-// collego il bottone a un evento
-const buttonGrid = document.getElementById("generate-grid");
-
-// collego il select
-const difficultySelect = document.getElementById("difficulty-select");
-
-buttonGrid.addEventListener("click", function () {
-  const difficulty = difficultySelect.value;
-  getGenerateGrid(difficulty);
-});
-
+/**
+ * funzione che genera la griglia
+ * @param {difficoltà} difficulty
+ */
 function getGenerateGrid(difficulty) {
   if (difficulty == "1") {
     // variabile cella inserita in un ciclo for
@@ -36,8 +26,12 @@ function getGenerateGrid(difficulty) {
     }
   }
 }
-
-// funzione che genera celle
+/**
+ * funzione che genera le celle
+ * @param {indice} i
+ * @param {difficoltà} difficulty
+ * @returns cella
+ */
 function createCell(i, difficulty) {
   // creo la mia cella
   const cell = document.createElement("div");
@@ -60,3 +54,36 @@ function createCell(i, difficulty) {
 
   return cell;
 }
+
+/**
+ * funzione che genera numeri random
+ * @param {min} numero minimo del range
+ * @param {max} numero massimo del range
+ * @returns numero
+ */
+function getRandomNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+}
+
+// utilizzo la funzione getRandomNumber per ottenere le mie bombe
+
+const randomNumber = getRandomNumber(1, 16);
+console.log(randomNumber);
+
+// creo la mia black list
+const blackList = [];
+
+// collego il contenitore della griglia
+const gridElement = document.getElementById("grid");
+
+// collego il bottone a un evento
+const buttonGrid = document.getElementById("generate-grid");
+
+// collego il select
+const difficultySelect = document.getElementById("difficulty-select");
+
+buttonGrid.addEventListener("click", function () {
+  const difficulty = difficultySelect.value;
+  getGenerateGrid(difficulty);
+});
